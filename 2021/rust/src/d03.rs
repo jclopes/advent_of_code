8eac4afa240ab7a1e68f05535908973dc6c4709f
joao <jc.lopes@gmail.com>
2021-12-26 06:52:58 +0100

    Improve code testability. Add logging.  * Make p1 and p2 into functions.  * Replace eprint! with env_logger

--- 2021/rust/src/d03.rs
+++ 2021/rust/src/d03.rs
@@ -1,6 +1,9 @@
+extern crate env_logger;
+
 use std::{
     io::{self, Read},
 };
+use log::info;
 
 const MASK: [u32; 12] = [
     0b100000000000,
@@ -18,25 +21,27 @@ const MASK: [u32; 12] = [
 ];
 
 pub fn run() {
-    let values: Vec<u32> = parse_input();
-
-    println!("## Day 3");
-    p1(values.as_slice());
-    println!();
-    p2(values);
-}
-
-fn parse_input() -> Vec<u32> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).expect("fail on read input");
+
+    let values: Vec<u32> = parse_input(input);
+
+    println!("## Day 3");
+    println!("__Part 1:__");
+    println!("output: {}", p1(values.as_slice()));
+    println!();
+    println!("__Part 2:__");
+    println!("output: {}", p2(values));
+}
+
+fn parse_input(input: String) -> Vec<u32> {
     let bnum = input.lines().map(
         |n| u32::from_str_radix(n, 2)
     ).collect::<Result<Vec<_>, _>>();
     return bnum.unwrap();
 }
 
-fn p1(values: &[u32]) {
-    let total = values.len();
+fn p1(values: &[u32]) -> u32 {
     let mut ones: [u32; 12] = [0; 12];
     for v in values {
         for i in 0..12 {
@@ -47,19 +52,20 @@ fn p1(values: &[u32]) {
     }
     let mut gamma: u32 = 0;
     let mut epsilon: u32 = 0;
+    let total: u32 = values.len() as u32;
     for i in 0..12 {
-        if ones[i] > (total as u32/ 2) {
+        if ones[i] > (total / 2) {
             gamma |= MASK[i];
         } else {
             epsilon |= MASK[i];
         }
     }
-    println!("__Part 1:__");
-    println!("{:012b} {:012b}", gamma, epsilon);
-    println!("{} x {} = **{}**", gamma, epsilon, gamma*epsilon);
+    info!("{:012b} {:012b}", gamma, epsilon);
+    info!("{} x {} = **{}**", gamma, epsilon, gamma*epsilon);
+    return gamma*epsilon;
 }
 
-fn p2(values: Vec<u32>) {
+fn p2(values: Vec<u32>) -> u32 {
     let mut oxygen: u32 = 0;
     let mut co2: u32 = 0;
     
@@ -95,6 +101,20 @@ fn p2(values: Vec<u32>) {
         }
     }
 
-    println!("__Part 2:__");
-    println!("O2 = {}\nCO2 = {}\nO2 x CO2 = **{}**", oxygen, co2, oxygen*co2);
+    info!("O2 = {}\nCO2 = {}\nO2 x CO2 = **{}**", oxygen, co2, oxygen*co2);
+    return oxygen*co2;
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = String::from("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n");
+        let values = parse_input(input);
+
+        assert_eq!(198, p1(values.as_slice()));
+    }
 }
--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -1,14 +1,17 @@
+extern crate env_logger;
+use log::error;
 use std::env;
 
 mod d03;
 
 fn main() {
+    env_logger::init();
     if let Some(arg) = env::args().nth(1) {
         match arg.as_str() {
             "3" => d03::run(),
-            _ => eprintln!("not implemented or invalid argument: {}", arg),
+            _ => error!("not implemented or invalid argument: {}", arg),
         };
     } else {
-        eprintln!("missing day argument");
+        error!("missing day argument");
     }
 }
